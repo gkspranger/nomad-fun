a07b10471f81fe95012fa33c7c37c75e389234e0
gkspranger <greg@spranger.us>
2023-01-27 09:41:00 -0500

    ok

--- javaapp/javaapp/src/javaapp/core.clj
+++ javaapp/javaapp/src/javaapp/core.clj
@@ -14,20 +14,29 @@
   (-> (prom/collector-registry)
       (prom-jvm/initialize)
       (prom/register
-       (prom/counter :app/greets))))
+       (prom/counter :app/total_num_requests
+                     {:description "the total number of requests"
+                      :labels [:appinst :appname :appport]}))))
 
 (defn hello-text
   []
   (str "app_name=" app-name "; app_instance=" app-instance "; app_port=" app-port))
 
+(defn inc-tot-reqs
+  []
+  (-> prom-reg
+      (prom/inc :app/total_num_requests {:appinst app-instance
+                                         :appport app-port
+                                         :appname app-name})))
+
 (defn respond-hello
   [_request]
-  (-> prom-reg
-      (prom/inc :app/greets))
+  (inc-tot-reqs)
   {:status 200 :body (hello-text)})
 
 (defn respond-metrics
   [_request]
+  (inc-tot-reqs)
   {:status 200 :body (prom-export/text-format prom-reg)})
 
 (def routes
